28e42d5768818e3a9bf8bd9206927ada763aa3d9
Amal Paul <iamamalpaul1367@gmail.com>
2025-11-24 12:02:46 +0530

    change app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,48 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/header.jsx";
-import Banner from "./pages/banner.jsx";
-import Intro from "./pages/intro.jsx";
-import Description from "./pages/description.jsx";
-import Bee3D from "./components/3DObject.jsx";
-import ThirdPage from "./pages/thirdPage.jsx";
-import Contact from "./pages/contact.jsx";
-import Footer from "./pages/footer.jsx";
+import React from 'react'
+import './App.css'
+import Header from './components/header.jsx';
+import Banner from './pages/banner.jsx';
+import Intro from './pages/intro.jsx';
+import Description from './pages/description.jsx';
+import Bee3D from './components/3DObject.jsx';
+import ThirdPage from './pages/thirdPage.jsx';
+import Contact from './pages/contact.jsx';
+import Footer from './pages/footer.jsx';
+import { RotatingLines } from 'react-loader-spinner'
 
 function App() {
-  const [loading, setLoading] = React.useState(true);
 
+  const[loader, setLoader] = React.useState(true);
+  
   React.useEffect(() => {
-    // Fires when ALL content (images, fonts, etc.) are loaded
-    window.addEventListener("load", () => {
-      setLoading(false);
-    });
-
-    return () => window.removeEventListener("load", () => {});
-  }, []);
+    setTimeout(() => {
+      setLoader(false)
+    }, 4000);
+  },[]);
 
   return (
     <>
-      {loading ? (
-        <div className="loader-container">
-          <div className="loader-wrapper">
-            <div className="logo-text">Cain Builders</div>
-            <div className="gold-line"></div>
-          </div>
-        </div>
-      ) : (
-        <>
-          <Header />
-          <Banner />
-          <Intro />
-          <Description />
-          <ThirdPage />
-          <Contact />
-          <Footer />
-          <Bee3D />
-        </>
-      )}
+      {loader ? ( 
+      <div className="loader-container">
+      <div className="loader-wrapper">
+      <div className="logo-text">Cain Builders</div>
+      <div className="gold-line"></div>
+    </div>
+    </div>
+    ): (
+      <>
+      <Header />
+      <Banner />
+      <Intro />
+      <Description />
+      <ThirdPage />
+      <Contact />
+      <Footer />
+      <Bee3D />
+      </>
+    )}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
